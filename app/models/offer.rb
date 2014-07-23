5cd63bbd3eb4b429c536b5408fab69ffc3724c20
Simon Weber <simon.weber@netflower.de>
2014-07-23 17:38:41 +0200

    Adds validation to offer model

--- app/models/offer.rb
+++ app/models/offer.rb
@@ -1,2 +1,8 @@
 class Offer < ActiveRecord::Base
+  validates :name, presence: true
+  validates :name, length: { minimum: 2 }
+  validates :description, length: { in: 10..200 }
+  validates :created_at, presence: true
+  validates :quantity, numericality: { greater_than: 0 }
+  validates :cost, numericality: { greater_than: 0 }
 end
