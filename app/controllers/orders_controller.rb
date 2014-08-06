c32c00f949e2b5b7f3efd7dbf9e0b8f3007fec24
Simon Weber <simon.weber@netflower.de>
2014-08-06 18:25:13 +0200

    Adds destroy to the orders before_action

--- app/controllers/orders_controller.rb
+++ app/controllers/orders_controller.rb
@@ -1,5 +1,5 @@
 class OrdersController < ApplicationController
-  before_action :set_order, only: [:show, :edit, :update]
+  before_action :set_order, only: [:show, :edit, :update, :destroy]
   before_action :authenticate_user!
 
 
